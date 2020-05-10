b3b469eeffe1937065b05990a7d20f21fe02b8ef
ZachGraceffa <zachgraceffa@gmail.com>
2020-05-09 23:34:51 -0500

    mothers day homepage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <div id="app" v-bind:style="gradient" />
+  <div id="app" v-bind:style="gradient">
+    <div id="content">
+      <img id="mom-pic" src="https://gdurl.com/fbqa" />
+      <h1>Happy Mother's Day!</h1>
+      <p>
+        In every way<br />
+        I wish I could train<br />
+        To Robinwood Lane<br />
+        Then I could seek<br />
+        To kiss your cheek<br />
+        But since we are in quarantine<br />
+        Expect a gift on the 14th
+      </p>
+      <p>
+        I love you,<br />
+        Zach
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +25,7 @@ export default {
   name: 'App',
   data: function() {
     return {
-      showHowTo: !window.location.pathname,
-      gradientSteps: window.location.pathname.split('/').filter(x => x)
+      gradientSteps: ["E25B6A", "dfddf1", "FFAEAA"]
     };
   },
   computed: {
@@ -28,5 +45,34 @@ html, body {
 
 #app {
   height: 100%;
+  text-align: center;
 }
+
+#content {
+  display: block;
+  position: relative;
+  top: 30%;
+  left: 50%;
+  max-height: 50%;
+  max-width: 30%;
+  transform: translate(-50%, -50%);
+}
+
+h1 {
+  color : seashell;
+  text-shadow: 2px 2px 5px red;
+}
+
+p {
+  font-size: 150%;
+  color: seashell;
+  text-shadow: 2px 2px 5px red;
+}
+
+#mom-pic {
+    height: 100%; 
+    width: 100%; 
+    object-fit: 'contain';
+}
+
 </style>
